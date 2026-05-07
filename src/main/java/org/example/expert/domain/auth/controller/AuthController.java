1d12766af9ecd33dc1de61bf74526d3347c71158
nongbu24 <pop524@naver.com>
2026-05-07 20:42:18 +0900

    레벨 1: ArgumentResolver
    /config 디렉토리에 WebMvcConfig 생성
    WebMvcConfigurer에 AuthUserArgumentResolver 등록
    AuthUserArgumentResolver에 @Component 어노테이션 붙여줌

--- src/main/java/org/example/expert/domain/auth/controller/AuthController.java
+++ src/main/java/org/example/expert/domain/auth/controller/AuthController.java
@@ -7,6 +7,7 @@ import org.example.expert.domain.auth.dto.request.SignupRequest;
 import org.example.expert.domain.auth.dto.response.SigninResponse;
 import org.example.expert.domain.auth.dto.response.SignupResponse;
 import org.example.expert.domain.auth.service.AuthService;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
